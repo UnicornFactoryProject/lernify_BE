1cc1805e0b25b28b7b8126a8770d55771309df7d
Precious Steve <precioussteve01@gmail.com>
2024-09-26 00:24:17 +0100

    chore: configure database connection settings

---
+++ app/utils/email_utils.py
@@ -0,0 +1,26 @@
+from fastapi_mail import FastMail, MessageSchema, MessageType
+from typing import Optional
+
+async def send_email(subject: str, recipients: list[str], template_name: str,  context: Optional[dict] = None):
+    from main import email_templates
+    from premailer import transform
+    retry = 2
+
+    message = MessageSchema(
+        subject=subject,
+        recipients=recipients,
+        subtype=MessageType.html
+    )
+
+    # Render the template with context
+    html = email_templates.get_template(template_name).render(context)
+    message.body = transform(html)
+
+    for i in range(retry):
+        try:
+            fm = FastMail(conf)
+            await fm.send_message(message)
+            print("Email sent successfully")
+            break
+        except Exception as e:
+            print(e)
